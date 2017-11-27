e74db87bc3008d33ca5b6a5a2ea50933959873de
Alexander Shapoval <ashapoms@hotmail.com>
2017-11-27 09:29:07 +0300

    scripts added

---
+++ AzureHybrid/AzureHybrid/DeployTemplateToAzure.ps1
@@ -0,0 +1,63 @@
+<#############################################################
+ #                                                           #
+ # DeployTemplateToAzure.ps1										 #
+ #                                                           #
+ #############################################################>
+
+<#
+ .Synopsis
+	The script creates a new resource group in Azure subscription and deploys resources based on template file and parameters file.
+
+ .Requirements
+	Azure PowerShell modules must be installed in order to run the script (https://www.microsoft.com/web/handlers/webpi.ashx/getinstaller/WindowsAzurePowershellGet.3f.3f.3fnew.appids).
+ .Parameter ResourceGroupLocation
+	Sets Azure region for the deployment. Default region is 'westeurope'.
+ .Parameter DeployIndex
+	Sets a number for the deployment iteration.
+ .Parameter ResourceGroupPrefix
+	Used to form resource group name and deployment name.  
+ .Parameter AzureUserName
+	Azure Active Directory tenant user name. This account is used to deploy all resources and should have necessary permissions. 
+ .Parameter AzureUserPassword
+	Azure Active Directory tenant password. 
+
+.Example
+     If no parameters are provided, default values are used.
+
+     .\DeployTemplateToAzure.ps1 
+
+.Example
+     This example creates 'Test-RG02' resource group in West Europe region and starts deployment with the name 'Test-RG-Dep02'.
+
+     .\DeployTemplateToAzure.ps1 -ResourceGroupLocation 'westeurope' -DeployIndex '02' -ResourceGroupPrefix 'Test-RG' -AzureUserName 'admin@mytenant.onmicrosoft.com' -AzureUserPassword 'P@ssw0rd!@#$%' 
+#>
+
+
+Param(
+	[string] $ResourceGroupLocation = "westeurope",
+	[string] $DeployIndex = "101",
+	[string] $ResourceGroupPrefix = "Test-RG",
+	[string] $AzureUserName = "andvis@contosomsspb.onmicrosoft.com",
+	[string] $AzureUserPassword = "@zureSt@ck"
+)
+
+# Prepare credentials and login to Azure subscription. 
+$AadPass = ConvertTo-SecureString $AzureUserPassword -AsPlainText -Force
+$AadCred = New-Object System.Management.Automation.PSCredential ($AzureUserName, $Aadpass)
+Login-AzureRmAccount -Credential $AadCred
+
+# Prepare environment variables.  
+$ResourceGroupName = $ResourceGroupPrefix + $DeployIndex
+$DeploymentName = $ResourceGroupPrefix + "-Dep" + $DeployIndex
+$TemplateUri = ""
+$TemplateParameterUri = ""
+
+# Create a new resource group in given region.  
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
+
+# Start a new deployment in created resource group using local files.
+New-AzureRmResourceGroupDeployment -Name $DeploymentName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -TemplateUri $TemplateUri `
+                                       -TemplateParameterUri $TemplateParametersUri `
+                                       -Verbose
---
+++ AzureHybrid/AzureHybrid/DeployTemplateToMAS.ps1
@@ -0,0 +1,89 @@
+﻿<#############################################################
+ #                                                           #
+ # DeployTemplate.ps1										 #
+ #                                                           #
+ #############################################################>
+
+<#
+ .Synopsis
+	The script creates a new resource group in Azure Stack subscription and deploys resources based on template file and parameters file.
+
+ .Requirements
+	Azure PowerShell modules must be installed in order to run the script (https://www.microsoft.com/web/handlers/webpi.ashx/getinstaller/WindowsAzurePowershellGet.3f.3f.3fnew.appids).
+ .Parameter ResourceGroupLocation
+	Sets Azure region for the deployment. Default region is 'westeurope'.
+ .Parameter DeployIndex
+	Sets a number for the deployment iteration.
+ .Parameter ResourceGroupPrefix
+	Used to form resource group name and deployment name.  
+ .Parameter AzureUserName
+	Azure Active Directory tenant user name. This account is used to deploy all resources and should have necessary permissions. 
+ .Parameter AzureUserPassword
+	Azure Active Directory tenant password. 
+
+.Example
+     If no parameters are provided, default values are used.
+
+     .\DeployTemplate.ps1 
+
+.Example
+     This example creates 'Open-RG02' resource group in West Europe region and starts deployment with the name 'Open-RG-Dep02'.
+
+     .\DeployTemplate.ps1 -ResourceGroupLocation 'westeurope' -DeployIndex '02' -ResourceGroupPrefix 'Open-RG' -AzureUserName 'admin@mytenant.onmicrosoft.com' -AzureUserPassword 'P@ssw0rd!@#$%' 
+#>
+
+
+Param(
+	[string] $ResourceGroupLocation = "local",
+	[string] $DeployIndex = "101",
+	[string] $ResourceGroupPrefix = "MktgDev-RG",
+	[string] $AzureUserName = "andvis@contosomsspb.onmicrosoft.com",
+	[string] $AzureUserPassword = "@zureSt@ck"
+)
+
+# Change to the tools directory
+cd E:\AzureStack-Tools-master
+
+# After downloading the tools, navigate to the downloaded folder and import the Connect PowerShell module by using the following command: 
+Import-Module .\Connect\AzureStack.Connect.psm1
+
+# Register an AzureRM environment that targets your Azure Stack instance
+Add-AzureRMEnvironment `
+  -Name "AzureStackUser" `
+  -ArmEndpoint "https://management.local.azurestack.external"
+
+# Set the GraphEndpointResourceId value
+Set-AzureRmEnvironment `
+  -Name "AzureStackUser" `
+  -GraphAudience "https://graph.windows.net/"
+
+# Get the Active Directory tenantId that is used to deploy Azure Stack
+$TenantID = Get-AzsDirectoryTenantId `
+  -AADTenantName "contosomsspb.onmicrosoft.com" `
+  -EnvironmentName "AzureStackUser"
+
+# Prepare credentials and login to Azure subscription. 
+$AadPass = ConvertTo-SecureString $AzureUserPassword -AsPlainText -Force
+$AadCred = New-Object System.Management.Automation.PSCredential ($AzureUserName, $Aadpass)
+
+# Use this command to sign-in to the user portal.
+Login-AzureRmAccount `
+  -EnvironmentName "AzureStackUser" `
+  -TenantId $TenantID `
+  -Credential $AadCred
+
+# Prepare environment variables.  
+$ResourceGroupName = $ResourceGroupPrefix + $DeployIndex
+$DeploymentName = $ResourceGroupPrefix + "-Dep" + $DeployIndex
+$TemplateFile = "E:\Distr\DevCon201706-master\MAS-ARM\MAS-ARM\azureHybrid.json"
+$TemplateParametersFile = "E:\Distr\DevCon201706-master\MAS-ARM\MAS-ARM\azureHybrid.parameters.json"
+
+# Create a new resource group in given region.  
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
+
+# Start a new deployment in created resource group using local files.
+New-AzureRmResourceGroupDeployment -Name $DeploymentName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -TemplateFile $TemplateFile `
+                                       -TemplateParameterFile $TemplateParametersFile `
+                                       -Verbose 
